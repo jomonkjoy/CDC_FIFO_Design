a8d2e529f0011c9f2d18f47c59b5fdec8fc07f9c
Jomon K Joy <jomonkurianjoy@gmail.com>
2017-12-13 20:42:44 +0000

    Update multibit_fifo_sync.sv

--- multibit_fifo_sync.sv
+++ multibit_fifo_sync.sv
@@ -13,7 +13,55 @@ module multibit_fifo_sync #(
   output logic [DATA_WIDTH-1:0] bdata,
   input  logic                  bready
 );
-
+  
+  logic wptr,r_wptr;
+  logic rptr,w_rptr;
+  // generate wptr using toggle-FF
+  always_ff @(posedge aclk) begin
+    if (areset) begin
+      wptr <= 1'b0;
+    end else begin
+      wptr <= wptr^(avalid&aready);
+    end
+  end
+  assign aready = ~(wptr^w_rptr);
+  // generate rptr using toggle-FF
+  always_ff @(posedge bclk) begin
+    if (breset) begin
+      rptr <= 1'b0;
+    end else begin
+      rptr <= rptr^(bvalid&bready);
+    end
+  end
+  assign bvalid = rptr^r_wptr;
+  // 1-deep DPRAM
+  multibit_fifo_dpram #(
+    .DATA_WIDTH(DATA_WIDTH)
+  ) multibit_fifo_dpram_innst (
+    .clk(aclk),
+    .wen(avalid&aready),
+    .waddr(wptr),
+    .wdata(adata),
+    .raddr(rptr),
+    .rdata(bdata)
+  );
+  // Sync rptr to aclk
+  multibit_fifo_double_sync
+  multibit_fifo_double_sync_w_rptr (
+    .clk(aclk),
+    .reset(areset),
+    .d(rptr),
+    .p(w_rptr)
+  );
+  // Sync wptr to bclk
+  multibit_fifo_double_sync
+  multibit_fifo_double_sync_r_wptr (
+    .clk(bclk),
+    .reset(breset),
+    .d(wptr),
+    .p(r_wptr)
+  );
+  
 endmodule
 
 module multibit_fifo_dpram #(

5e5241776aab19e02ee14d15be428baaefb8cebc
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-01-31 13:25:01 +0000

    Update cpu_if_cdc.sv

--- cpu_if_cdc.sv
+++ cpu_if_cdc.sv
@@ -23,4 +23,40 @@ module cpu_if_cdc #(
   input  logic                  cpu_m_access_complete
 );
   
+  logic cpu_s_access_valid;
+  logic cpu_m_access_valid;
+  
+  multibit_mcp_sync #(
+    .DATA_WIDTH (ADDR_WIDTH+DATA_WIDTH+1)
+  ) sync_request (
+    .aclk   (cpu_s_clk),
+    .areset (cpu_s_reset),
+    .avalid (cpu_s_access_valid),
+    .adata  ({cpu_s_write,cpu_s_address,cpu_s_write_data}),
+    .aready (cpu_s_access_ready),
+    .bclk   (cpu_m_clk),
+    .breset (cpu_m_reset),
+    .bvalid (cpu_m_access_valid),
+    .bdata  ({cpu_m_write,cpu_m_address,cpu_m_write_data}),
+    .bready (cpu_m_access_ready)
+  );
+  
+  assign cpu_s_access_valid = cpu_s_write|cpu_s_read;
+  assign cpu_m_read = cpu_m_access_valid&(~cpu_m_write);
+  
+  multibit_mcp_sync #(
+    .DATA_WIDTH (DATA_WIDTH)
+  ) sync_response (
+    .aclk   (cpu_m_clk),
+    .areset (cpu_m_reset),
+    .avalid (cpu_m_access_complete),
+    .adata  (cpu_m_read_data),
+    .aready (),
+    .bclk   (cpu_s_clk),
+    .breset (cpu_s_reset),
+    .bvalid (cpu_s_access_complete),
+    .bdata  (cpu_s_read_data),
+    .bready (1'b1)
+  );
+  
 endmodule

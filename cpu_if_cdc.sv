8fa3d564e5c10f0095759aab9c6871f8dd415cab
Jomon K Joy <jomonkurianjoy@gmail.com>
2018-01-31 13:00:34 +0000

    Update cpu_if_cdc.sv

--- cpu_if_cdc.sv
+++ cpu_if_cdc.sv
@@ -1,4 +1,26 @@
-module cpu_if_cdc (
+module cpu_if_cdc #(
+  parameter ADDR_WIDTH = 32,
+  parameter DATA_WIDTH = 32
+) (
+  input  logic                  cpu_s_clk,
+  input  logic                  cpu_s_reset,
+  input  logic                  cpu_s_write,
+  input  logic                  cpu_s_read,
+  input  logic [ADDR_WIDTH-1:0] cpu_s_address,
+  input  logic [DATA_WIDTH-1:0] cpu_s_write_data,
+  output logic [DATA_WIDTH-1:0] cpu_s_read_data,
+  output logic                  cpu_s_access_ready,
+  output logic                  cpu_s_access_complete,
+  
+  input  logic                  cpu_m_clk,
+  input  logic                  cpu_m_reset,
+  output logic                  cpu_m_write,
+  output logic                  cpu_m_read,
+  output logic [ADDR_WIDTH-1:0] cpu_m_address,
+  output logic [DATA_WIDTH-1:0] cpu_m_write_data,
+  input  logic [DATA_WIDTH-1:0] cpu_m_read_data,
+  input  logic                  cpu_m_access_ready,
+  input  logic                  cpu_m_access_complete
 );
-
+  
 endmodule
